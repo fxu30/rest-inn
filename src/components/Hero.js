9ae8e4d473fbeefc3d4580ee12d42064c6a54d4e
Frank Xu <xfmedu00@gmail.com>
2022-03-10 06:38:17 -0500

    hero created

---
+++ src/components/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import icon from '../assets/img/icon.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide><img src={icon} alt=''/></SwiperSlide>
+      <SwiperSlide>Slide 2<img src={icon} alt=''/></SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+    </div>
+  )
+}
+
+export default Hero
