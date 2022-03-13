2267cb6aaede3cdaf0ef7322f81689cf83968e7e
Frank Xu <xfmedu00@gmail.com>
2022-03-13 05:30:39 -0400

    scrolltotop added

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,14 @@ import LoginPage from '../pages/LoginPage';
 import PropertyDescriptionPage from '../pages/PropertyDescriptionPage';
 import PropertyListingPage from '../pages/PropertyListingPage';
 import RegistrationPage from '../pages/RegistrationPage';
+import ScrollToTop from '../components/ScrollToTop'
 
 
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Detail" element={<PropertyDescriptionPage />} />
@@ -23,6 +25,7 @@ function App() {
         <Route path="/Login" element={<LoginPage />} />
         <Route path='/Listing' element={<PropertyListingPage />} />
       </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
