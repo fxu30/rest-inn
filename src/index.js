28631461f1eab1068be4ef8a54b54d79d4a66c16
Frank Xu <xfmedu00@gmail.com>
2022-04-21 16:55:00 -0400

    redux config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 
 import 'foundation-sites/dist/css/foundation.css';
@@ -8,6 +10,8 @@ import './assets/css/App.css'
 
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/redux/apiCalls.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { 
+    loginStart,
+    loginSuccess,
+    loginFailure,
+    logoutStart,
+    logoutSuccess,
+    logoutFailure } from "./userRedux"
+import configData from "../config.json"
+
+export const login = async (dispatch, user) => {
+    dispatch(loginStart());
+    try{
+        const res = await axios.post(`${configData.SERVER_URL}/Auth`, user)
+        dispatch(loginSuccess(res.data))
+    }catch(err){
+        dispatch(loginFailure())
+    }
+}
+
+export const logout = async (dispatch) => {
+    dispatch(logoutStart());
+    try {
+      const res = await axios.get(`${configData.SERVER_URL}/Auth`);
+      dispatch(logoutSuccess(res.data));
+    } catch (err) {
+      dispatch(logoutFailure());
+    }
+  };
